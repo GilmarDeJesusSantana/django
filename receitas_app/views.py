8786c5787109c7a8dee41f75dc1807f6df06f399
Gilmar <gilmar.jesus@gmail.com>
2022-02-27 10:41:32 -0300

    Exibindo as receitas na pagina de receitas.
    Alterado a rota para buscar o id da receita.
    Implentado a capitura dos dados pela view.
    Ajustado as pagingas para exibir as receitas.Alterado a url para
    
    Please enter the commit message for your changes. Lines starting

--- receitas_app/views.py
+++ receitas_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import  Receita
+from django.shortcuts import render, get_object_or_404
+from .models import Receita
+
 
 def index(request):
     receitas = Receita.objects.all()
@@ -9,5 +10,7 @@ def index(request):
     return render(request, 'index.html', dados)
 
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    receita_a_exibir = {'receita': receita}
+    return render(request, 'receita.html', receita_a_exibir)
