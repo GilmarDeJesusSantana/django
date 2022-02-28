81b785f84478ba2e17c77e214584b26facf22a6a
Gilmar <gilmar.jesus@gmail.com>
2022-02-28 14:12:20 -0300

    Ativado funcionalidade de upload de imagens.

--- receitas_app/views.py
+++ receitas_app/views.py
@@ -3,7 +3,7 @@ from .models import Receita
 
 
 def index(request):
-    receitas = Receita.objects.filter(publicado=True)
+    receitas = Receita.objects.order_by('-date_receita').filter(publicado=True)
     dados = {
         'receitas': receitas
     }
