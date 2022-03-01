0dfb94a6fe6839b23eed425e38eda05b57a5812f
Gilmar <gilmar.jesus@gmail.com>
2022-03-01 11:08:12 -0300

    Criado formularios de cadastro e login.
    Criado rotas para os formularios.

--- receitas/settings.py
+++ receitas/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'receitas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'receitas_app/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
