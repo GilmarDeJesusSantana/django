6f4b5126b942e3fd2e9475028c08257c652a77ac
Gilmar <gilmar.jesus@gmail.com>
2022-02-28 22:24:45 -0300

    Refatorado os nomes dos templates partials.

--- receitas_app/models.py
+++ receitas_app/models.py
@@ -17,3 +17,5 @@ class Receita(models.Model):
     date_receita = models.DateTimeField(default=datetime.now, blank=True)
     foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
     publicado = models.BooleanField(default=False)
+    def __str__(self):
+        return self.nome_receita
